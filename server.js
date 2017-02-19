0c090f2cdcc531923e68344db75238bc1492672c
Mayur01 <mayurkhomane01@gmail.com>
2017-02-19 16:57:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,11 +13,6 @@ app.get('/article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', '/article-one.html'));
 });
 
-app.get('/ui/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-});
-
-
 app.get('/article-two', function (req, res){
     res.sendFile(path.join(__dirname, 'ui','/article-two.html'));
 });
