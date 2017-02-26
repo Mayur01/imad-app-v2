c8f72d4da626d3238e1d8f559e679787245b834f
Mayur01 <mayurkhomane01@gmail.com>
2017-02-26 11:07:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -79,6 +79,7 @@ app.get('/', function (req, res) {
 app.get('/:articleName', function (req, res) {
     //articleName == article-one,article-two
    // articles[articleName] == {} content obj for articles
+   var articleName=res.params.articleName;
   res.send(createTemplate(articles[articleName]));
 });
 
