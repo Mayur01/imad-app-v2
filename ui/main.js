82e15cb9ed26f47da1283214435a0f646500db81
Mayur01 <mayurkhomane01@gmail.com>
2017-02-27 12:16:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,9 @@
 console.log('Loaded!');
-
-// moving image.
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight () {
-    marginLeft = marginLeft + 5;
-    img.style.marginleft = marginLeft + 'px';
-}
-img.onclick = function () {
-    var interval = setInterval(moveRight,50);
+var button = document.getElementById("counter");
+var counter =0;
+button.onclick= function() {
+  //render the variable in span
+  counter =counter+1;
+  var span = document.getElementById("count");
+  span.innerHTML=counter.toString();
 };
